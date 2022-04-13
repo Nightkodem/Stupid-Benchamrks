f5224ec919b9a7751b983e95427931fc851db2b0
236559 Kirsz Nikodem <236559@edu.p.lodz.pl>
2022-04-13 13:48:23 +0200

    Array vs Enumerable vs HashSet Contains

--- Program.cs
+++ Program.cs
@@ -9,6 +9,6 @@ class Program
     {
         /*new IntPower().Test();
         Console.ReadLine();*/
-        var summary = BenchmarkRunner.Run<IntPower>();
+        var summary = BenchmarkRunner.Run<ArrayContains_Vs_HashSet>();
     }
 }
